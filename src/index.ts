da8d0d42bbd857496fff254cfa94680d7e10d480
Dave Uczen <dave@duczen.io>
2021-08-13 00:43:59 -0600

    debug

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as core from '@actions/core';
+import { context, getOctokit } from '@actions/github';
+import { captureRejectionSymbol } from 'events';
 
 //const github = require('@actions/github');
 //const http = require('@actions/http-client');
@@ -12,26 +14,83 @@ async function run(): Promise<void> {
     const runId = process.env.GITHUB_RUN_ID;
     const runNumber = process.env.GITHUB_RUN_NUMBER;
     const githubServer = process.env.GITHUB_SERVER_URL;
-    const repo = process.env.GITHUB_REPOSITORY;
-    const buildURL = `${githubServer}/${repo}/actions/runs/${runId}`;
+    const repoName = process.env.GITHUB_REPOSITORY;
+    const buildURL = `${githubServer}/${repoName}/actions/runs/${runId}`;
     const workflow = process.env.GITHUB_WORKFLOW;
     const actor = process.env.GITHUB_ACTOR;
     const actorURL = `${githubServer}/${actor}`;
 
-    core.debug(`server: ${server}`);
-    core.debug(`room_id: ${roomId}`);
-    core.debug(`token: ${token}`);
     core.debug(`status: ${status}`);
     core.debug(`runId: ${runId}`);
     core.debug(`runNumber: ${runNumber}`);
     core.debug(`githubServer: ${githubServer}`);
+    core.debug(`repoName: ${repoName}`)
     core.debug(`buildURL: ${buildURL}`)
     core.debug(`workflow: ${workflow}`)
     core.debug(`actor: ${actor}`)
     core.debug(`actorURL: ${actorURL}`)
 
-    //post(server, room_id, token);
+    const { owner, repo } = context.repo;
+    const { sha: ref } = context;
+    const octokit = getOctokit(token);
+    const resp = await octokit.rest.repos.getCommit({owner, repo, ref});
 
+    core.debug(`resp.data.html_url: ${resp.data.html_url}`);
+    core.debug(`resp.data.commit.message: ${resp.data.commit.message}`);
+    core.debug(`resp.data.commit.author: ${resp.data.commit.author}`)
+    const author = resp.data.commit.author;
+    core.debug(`author.name: ${author?.name}`)
+    core.debug(`author.email: ${author?.email}`)
+
+    const resp2 = await octokit.rest.actions.listJobsForWorkflowRun({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      run_id: context.runId,
+    });
+    const jobName= context.job;
+    const currentJob = resp2?.data.jobs.find(job => job.name === jobName);
+    if (currentJob) {
+      let time = new Date().getTime() - new Date(currentJob.started_at).getTime();
+
+      const h = Math.floor(time / (1000 * 60 * 60));
+      time -= h * 1000 * 60 * 60;
+      const m = Math.floor(time / (1000 * 60));
+      time -= m * 1000 * 60;
+      const s = Math.floor(time / 1000);
+
+      let value = '';
+      if (h > 0) {
+        value += `${h} hour `;
+      }
+      if (m > 0) {
+        value += `${m} min `;
+      }
+      if  (s > 0) {
+        value += `${s} sec`;
+      }
+      core.debug(`value: ${value}`)
+      const jobId = currentJob.id;
+
+      const url = `<https://github.com/${owner}/${context.repo.repo}/runs/${jobId}|${jobName}>`;
+      //post(server, room_id, token);
+      console.log(`url: ${url}`)
+      const sha = context.sha
+      const url2 = `<https://github.com/${owner}/${repo}/commit/${sha}|${sha.slice(0, 8)}>`;
+      console.log(`url2: ${url2}`)
+      const url3 = `<https://github.com/${owner}/${repo}|${owner}/${repo}>`;
+      console.log(`url3: ${url3}`)
+      console.log(`context.event: ${context.eventName}`)
+      console.log(`context.ref: ${context.ref}`)
+
+      const sha1 = context.payload.pull_request?.head.sha ?? context.sha;
+      const url4 = `<https://github.com/${owner}/${repo}/commit/${sha1}/checks|${context.workflow}>`;
+      console.log(`url4: ${url4}`)
+
+      const sha2 = context.payload.pull_request?.head.sha ?? context.sha;
+      const url5 = `<https://github.com/${owner}/${repo}/commit/${sha2}/checks|action>`;
+      console.log(`url5: ${url5}`)
+
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
